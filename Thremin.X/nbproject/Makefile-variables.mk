5bd07265ce977ab91a7b8c1a6186a83edd56153d
Kraisenberg <matias.sacchi@mi.unc.edu.ar>
2025-10-28 16:49:42 -0300

    Preparaciones inicales

---
+++ Thremin.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=Thremin.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/Thremin.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=thremin.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/thremin.x.tar
